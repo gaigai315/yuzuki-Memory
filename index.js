88fdcf5faa3c003e59aaf74c126684ac12a8b226
gaigai315 <www.512204242@qq.com>
2026-07-16 23:18:33 +0800

    更正版本号为0.5.0

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
     'use strict';
 
     const NAMESPACE = 'YuzukiMemory';
-    const VERSION = '0.4.10';
+    const VERSION = '0.5.0';
     const baseUrl = new URL('./', import.meta.url).href;
 
     const MODULES = [
